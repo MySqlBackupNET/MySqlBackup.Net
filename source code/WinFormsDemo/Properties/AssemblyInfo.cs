6d641a1161e201bec6c73da655efec08d7a7d911
adriancs <adriancs202@gmail.com>
2025-08-05 01:05:59 +0800

    Update assembly version to 2.6.4

--- source code/WinFormsDemo/Properties/AssemblyInfo.cs
+++ source code/WinFormsDemo/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.6.5.0")]
+[assembly: AssemblyFileVersion("2.6.5.0")]
